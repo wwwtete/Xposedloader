beacef9fe490ba505f2c9b102aa1f67f065bc17e
wangw <wangwei2@rongcloud.cn>
2019-08-06 14:44:11 +0800

    Mod:解决PackageManagerService找不到问题

--- xp_sysutils/src/main/java/ro/xposed/xp_sysutils/MainHook.java
+++ xp_sysutils/src/main/java/ro/xposed/xp_sysutils/MainHook.java
@@ -23,8 +23,8 @@ public class MainHook implements IXposedHookLoadPackage {
 
     public MainHook(){
          mSp = new XSharedPreferences("ro.xposed.xp_sysutils",MainActivity.SPNAME);
-         mSp.reload();
-         log("backup="+mSp.getBoolean(MainActivity.KEY_BACKUP,false)+", debug="+ mSp.getBoolean(MainActivity.KEY_DEBUG,false));
+//         mSp.reload();
+//         log("backup="+mSp.getBoolean(MainActivity.KEY_BACKUP,false)+", debug="+ mSp.getBoolean(MainActivity.KEY_DEBUG,true));
     }
 
     /**
@@ -33,26 +33,36 @@ public class MainHook implements IXposedHookLoadPackage {
      * @throws Throwable
      */
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        mSp.reload();
-        log("backup="+mSp.getBoolean(MainActivity.KEY_BACKUP,false)+", debug="+ mSp.getBoolean(MainActivity.KEY_DEBUG,false));
-        if (!mSp.getBoolean(MainActivity.KEY_BACKUP,false) && !mSp.getBoolean(MainActivity.KEY_DEBUG,false))
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+//        mSp.reload();
+//        log("backup="+mSp.getBoolean(MainActivity.KEY_BACKUP,false)+", debug="+ mSp.getBoolean(MainActivity.KEY_DEBUG,true));
+//        if (!mSp.getBoolean(MainActivity.KEY_BACKUP,false) && !mSp.getBoolean(MainActivity.KEY_DEBUG,true))
+//            return;
+//        log("start find="+lpparam.packageName);
+        if (!lpparam.packageName.equals("android")){
             return;
-
-        XposedBridge.hookAllMethods(XposedHelpers.findClass("com.android.server.pm.PackageManagerService", lpparam.classLoader), "getPackageInfo", new XC_MethodHook() {
+        }
+        log("start Hook getPackageInfo");
+        Class<?> clz = XposedHelpers.findClass("com.android.server.pm.PackageManagerService", lpparam.classLoader);
+//        log("find PackageManagerService success!");
+        XposedBridge.hookAllMethods(clz, "getPackageInfo", new XC_MethodHook() {
 
             public void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+//                log("afterHookedMethod ==>");
                 PackageInfo packageInfo = (PackageInfo) param.getResult();
                 if (packageInfo != null) {
                     ApplicationInfo appinfo = packageInfo.applicationInfo;
                     int flags = appinfo.flags;
-                    log( "Load App : " + appinfo.packageName);
+                    mSp.reload();
+                    boolean isDebug = mSp.getBoolean(MainActivity.KEY_DEBUG, false);
+                    boolean isBackup = mSp.getBoolean(MainActivity.KEY_BACKUP, false);
+                    log( "Load App : " + appinfo.packageName+" , isDebug="+isDebug+" , isBackup="+isBackup);
                     log( "==== After Hook ====");
 
-                    if (mSp.getBoolean(MainActivity.KEY_BACKUP,false) && (flags & 32768) == 0) {
+                    if (isBackup && (flags & 32768) == 0) {
                         flags |= 32768;
                     }
-                    if (mSp.getBoolean(MainActivity.KEY_DEBUG,false) && (flags & 2) == 0) {
+                    if (isDebug && (flags & 2) == 0) {
                         flags |= 2;
                     }
                     appinfo.flags = flags;
